ccfc3534a3d79942c76bb7ed6394465bdcefe3af
sonalcv4 <sonalcv@gmail.com>
2026-02-04 16:33:47 +0530

    nav-block

--- blocks/cat/cat.js
+++ blocks/cat/cat.js
@@ -1,64 +1,36 @@
-// import { createOptimizedPicture } from '../../scripts/aem.js';
-
-// export default function decorate(block) {
-
-//   const ul = document.createElement('ul');
-
-//   [...block.children].forEach((row) => {
-
-//     // EACH COLUMN becomes a card
-//     [...row.children].forEach((col) => {
-
-//       const li = document.createElement('li');
-
-//       const pic = col.querySelector('picture');
-//       if (pic) {
-//         pic.classList.add('cat-card-image');
-//         li.append(pic);
-//       }
-
-//       ul.append(li);
-//     });
-
-//   });
-
-//   // Optimize images (same as cards block)
-//   ul.querySelectorAll('picture > img').forEach((img) => {
-//     img.closest('picture').replaceWith(
-//       createOptimizedPicture(img.src, img.alt || '', false, [{ width: '750' }])
-//     );
-//   });
-
-//   block.replaceChildren(ul);
-// }
-
-
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-/**
- * @param {Element} block The cat block element
- */
 export default function decorate(block) {
-  // Find all images in the block
-  const images = [...block.querySelectorAll('img')];
-  
-  // Clear the block's initial table-like structure
-  block.textContent = '';
 
-  // Create a container for the horizontal grid
-  const container = document.createElement('div');
-  container.classList.add('cat-grid-container');
+  const ul = document.createElement('ul');
 
-  images.forEach((img) => {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('cat-image-wrapper');
-    
-    // Use the optimized picture utility from your reference
-    const picture = createOptimizedPicture(img.src, img.alt || 'Cat image', false, [{ width: '750' }]);
-    
-    wrapper.append(picture);
-    container.append(wrapper);
+  // Loop through rows
+  [...block.children].forEach((row) => {
+
+    // Loop through each column
+    [...row.children].forEach((col) => {
+
+      const li = document.createElement('li');
+
+      const picture = col.querySelector('picture');
+
+      if (picture) {
+        picture.classList.add('cat-card-image');
+        li.append(picture);
+        ul.append(li);
+      }
+
+    });
+
   });
 
-  block.append(container);
+  // Optimize images (same as cards block)
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    img.closest('picture').replaceWith(
+      createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])
+    );
+  });
+
+  block.textContent = '';
+  block.append(ul);
 }
