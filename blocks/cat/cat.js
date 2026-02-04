f5e441c7c60273b3fe34457b4dd42b2934b39172
sonalcv4 <sonalcv@gmail.com>
2026-02-04 14:18:05 +0530

    he update

--- blocks/cat/cat.js
+++ blocks/cat/cat.js
@@ -1,34 +1,64 @@
+// import { createOptimizedPicture } from '../../scripts/aem.js';
+
+// export default function decorate(block) {
+
+//   const ul = document.createElement('ul');
+
+//   [...block.children].forEach((row) => {
+
+//     // EACH COLUMN becomes a card
+//     [...row.children].forEach((col) => {
+
+//       const li = document.createElement('li');
+
+//       const pic = col.querySelector('picture');
+//       if (pic) {
+//         pic.classList.add('cat-card-image');
+//         li.append(pic);
+//       }
+
+//       ul.append(li);
+//     });
+
+//   });
+
+//   // Optimize images (same as cards block)
+//   ul.querySelectorAll('picture > img').forEach((img) => {
+//     img.closest('picture').replaceWith(
+//       createOptimizedPicture(img.src, img.alt || '', false, [{ width: '750' }])
+//     );
+//   });
+
+//   block.replaceChildren(ul);
+// }
+
+
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
+/**
+ * @param {Element} block The cat block element
+ */
 export default function decorate(block) {
-  const ul = document.createElement('ul');
+  // Find all images in the block
+  const images = [...block.querySelectorAll('img')];
+  
+  // Clear the block's initial table-like structure
+  block.textContent = '';
 
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
+  // Create a container for the horizontal grid
+  const container = document.createElement('div');
+  container.classList.add('cat-grid-container');
 
-    // Move all columns (images) into li
-    while (row.firstElementChild) {
-      li.append(row.firstElementChild);
-    }
-
-    // Mark image containers
-    [...li.children].forEach((div) => {
-      if (div.querySelector('picture')) {
-        div.className = 'cat-card-image';
-      }
-    });
-
-    ul.append(li);
+  images.forEach((img) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('cat-image-wrapper');
+    
+    // Use the optimized picture utility from your reference
+    const picture = createOptimizedPicture(img.src, img.alt || 'Cat image', false, [{ width: '750' }]);
+    
+    wrapper.append(picture);
+    container.append(wrapper);
   });
 
-  // Optimize images (same as cards)
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    img.closest('picture').replaceWith(
-      createOptimizedPicture(img.src, img.alt || '', false, [
-        { width: '750' },
-      ])
-    );
-  });
-
-  block.replaceChildren(ul);
+  block.append(container);
 }
